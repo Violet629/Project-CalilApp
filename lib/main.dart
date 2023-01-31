ebcff07022e90971055db68e09ba089f356747d0
violet629 <violet4649@g.yju.ac.kr>
2023-01-31 21:17:54 +0900

    스켓폴트적용

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('title'),
-        ),
+        // appBar: AppBar(
+        //   title: Text('title'),
+        // ),
         body: Center(
-          child: Text('body'),
+          child: Image(image: AssetImage('assets/calil_logo.png')),
         ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            height: 50.0,
-            color: Colors.lightBlue,
-          ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'ホーム',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_library),
+              label: '図書館',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_sharp),
+              label: '本',
+            ),
+          ],
+          // currentIndex: , // 지정 인덱스로 이동
+          selectedItemColor: Colors.blue,
+          // onTap: _onItemTapped, // 선언했던 onItemTapped
         ),
       ),
     );
