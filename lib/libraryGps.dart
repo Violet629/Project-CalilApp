d1102b7b727b88e314e658e11c2dd2650138db49
violet629 <violet4649@g.yju.ac.kr>
2023-02-28 16:16:02 +0900

    경도위도로도서관가져오기성공

--- lib/libraryGps.dart
+++ lib/libraryGps.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LibraryGps extends StatefulWidget {
   const LibraryGps({Key? key}) : super(key: key);
@@ -12,38 +15,85 @@ class LibraryGps extends StatefulWidget {
 
 class LibraryGpsState extends State<LibraryGps> {
   final Completer<GoogleMapController> _controller = Completer();
+  String _latitude = "NoData";
+  String _longitude = "NoData";
+  String key = "419e52784761e9f60fa6683a2f28e41e";
+  List<dynamic> libraryData = [];
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  Future<void> getLocation() async {
+    // 権限を取得
+    LocationPermission permission = await Geolocator.requestPermission();
+    // 権限がない場合は戻る
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
+    }
+    // 位置情報を取得
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(
+      () {
+        // 北緯がプラス、南緯がマイナス
+        _latitude = position.latitude.toStringAsFixed(2);
+        // 東経がプラス、西経がマイナス
+        _longitude = position.longitude.toStringAsFixed(2);
+        getLibraryData();
+      },
+    );
+  }
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  getLibraryData() async {
+    var result = await http.get(Uri.parse(
+        'https://api.calil.jp/library?appkey=$key&geocode=$_longitude,$_latitude&format=json&callback='));
+    if (result.statusCode == 200) {
+      setState(() {
+        var result2 = [jsonDecode(result.body)];
+        libraryData = result2[0];
+        print(libraryData);
+      });
+    } else {
+      libraryData.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/calil_logo_black.png',
+          width: 170,
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.lightBlueAccent,
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+      body: Column(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlueAccent,
+              minimumSize: const Size.fromHeight(50),
+            ),
+            onPressed: getLocation,
+            child: const Text(
+              "周りを検索",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
       ),
     );
   }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
 }
+// body: GoogleMap(
+//   mapType: MapType.hybrid,
+//   initialCameraPosition: _kGooglePlex,
+//   onMapCreated: (GoogleMapController controller) {
+//     _controller.complete(controller);
+//   },
+// ),
+// static const CameraPosition _kGooglePlex = CameraPosition(
+//   target: LatLng(35.71574708017275, 139.6086487826241),
+//   zoom: 35,
+// );
