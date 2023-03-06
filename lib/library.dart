bfb57e6b208a229d3166375e7650692bf005d328
violet629 <violet4649@g.yju.ac.kr>
2023-03-07 00:28:55 +0900

    인터페이스대폭수정중

--- lib/library.dart
+++ lib/library.dart
@@ -4,6 +4,10 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:kariru/store.dart';
 import 'package:kariru/resultLibrary.dart';
+import 'package:kariru/book.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/link.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Library extends StatefulWidget {
   const Library({Key? key}) : super(key: key);
@@ -13,6 +17,165 @@ class Library extends StatefulWidget {
 }
 
 class _LibraryState extends State<Library> {
+  // List prefCityList = [];
+  // String prefNum = "";
+  // List<Color> colorList = [
+  //   Colors.lightBlueAccent,
+  //   Colors.lightBlue,
+  //   Colors.blue,
+  //   Colors.blueAccent,
+  // ];
+  //
+  // setPrefNum(payload) {
+  //   setState(() {
+  //     prefNum = payload;
+  //     print(prefNum);
+  //   });
+  // }
+  //
+  // Future<void> readJson() async {
+  //   final String response =
+  //       await rootBundle.loadString('assets/pref_city.json');
+  //   final data = await json.decode(response);
+  //   setState(() {
+  //     prefCityList = [data[0]];
+  //   });
+  // }
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   readJson();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    final Store store = Provider.of<Store>(context, listen: true);
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Image.asset(
+      //     'assets/calil_logo_black.png',
+      //     width: 170,
+      //   ),
+      //   centerTitle: true,
+      //   automaticallyImplyLeading: false,
+      //   backgroundColor: Colors.lightBlueAccent,
+      // ),
+      // body: ListView.builder(
+      //   padding: const EdgeInsets.symmetric(horizontal: 2.5),
+      //   shrinkWrap: true,
+      //   itemCount: prefCityList[0].length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return Container(
+      //       margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+      //       padding: EdgeInsets.fromLTRB(0, 0, 0, 0.5),
+      //       child: Column(
+      //         children: [
+      //           ElevatedButton(
+      //             style: ElevatedButton.styleFrom(
+      //               primary: colorList[index % colorList.length],
+      //               minimumSize: const Size.fromHeight(50), // NEW
+      //             ),
+      //             onPressed: () {
+      //               setPrefNum('${index + 1}');
+      //               store.setPerf(prefCityList[0]['${index + 1}']["name"]);
+      //               Navigator.push(
+      //                 context,
+      //                 MaterialPageRoute(
+      //                   builder: (context) => LibraryCity(
+      //                       prefNum: prefNum, prefCityList: prefCityList),
+      //                 ),
+      //               );
+      //             },
+      //             child: Text(
+      //               prefCityList[0]['${index + 1}']["name"],
+      //               style: TextStyle(fontSize: 20),
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     );
+      //   },
+      // ),
+      body: LibraryStep(),
+    );
+  }
+}
+
+// class LibraryCity extends StatefulWidget {
+//   const LibraryCity({Key? key, this.prefNum, this.prefCityList})
+//       : super(key: key);
+//   final prefNum;
+//   final prefCityList;
+//
+//   @override
+//   State<LibraryCity> createState() => _LibraryCityState();
+// }
+//
+// class _LibraryCityState extends State<LibraryCity> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final Store store = Provider.of<Store>(context, listen: true);
+//     return Scaffold(
+//       // appBar: AppBar(
+//       //   title: Image.asset(
+//       //     'assets/calil_logo_black.png',
+//       //     width: 170,
+//       //   ),
+//       //   centerTitle: true,
+//       //   automaticallyImplyLeading: false,
+//       //   backgroundColor: Colors.lightBlueAccent,
+//       // ),
+//       body: ListView.builder(
+//         padding: const EdgeInsets.symmetric(horizontal: 2.5),
+//         shrinkWrap: true,
+//         itemCount: widget.prefCityList[0][widget.prefNum]['city'].length,
+//         itemBuilder: (BuildContext context, int index) {
+//           return Container(
+//             margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+//             padding: EdgeInsets.fromLTRB(0, 0, 0, 0.5),
+//             child: Column(
+//               children: [
+//                 ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     // primary: colorList[index % colorList.length],
+//                     minimumSize: const Size.fromHeight(50), // NEW
+//                   ),
+//                   onPressed: () {
+//                     store.setCity(widget.prefCityList[0][widget.prefNum]["city"]
+//                         [index]['city']);
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => ResultLibrary(
+//                             perfName: store.perfName, cityName: store.cityName),
+//                       ),
+//                     );
+//                   },
+//                   child: Text(
+//                     widget.prefCityList[0][widget.prefNum]["city"][index]
+//                         ['city'],
+//                     style: TextStyle(fontSize: 20),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class LibraryStep extends StatefulWidget {
+  const LibraryStep({Key? key}) : super(key: key);
+
+  @override
+  State<LibraryStep> createState() => _LibraryStepState();
+}
+
+class _LibraryStepState extends State<LibraryStep> {
+  var step = 0;
   List prefCityList = [];
   String prefNum = "";
   List<Color> colorList = [
@@ -22,12 +185,12 @@ class _LibraryState extends State<Library> {
     Colors.blueAccent,
   ];
 
-  setPrefNum(payload) {
-    setState(() {
-      prefNum = payload;
-      print(prefNum);
-    });
-  }
+  // setPrefNum(payload) {
+  //   setState(() {
+  //     prefNum = payload;
+  //     print(prefNum);
+  //   });
+  // }
 
   Future<void> readJson() async {
     final String response =
@@ -46,18 +209,8 @@ class _LibraryState extends State<Library> {
 
   @override
   Widget build(BuildContext context) {
-    final Store store = Provider.of<Store>(context, listen: true);
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/calil_logo_black.png',
-          width: 170,
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: ListView.builder(
+    if (step == 0) {
+      return ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 2.5),
         shrinkWrap: true,
         itemCount: prefCityList[0].length,
@@ -73,15 +226,12 @@ class _LibraryState extends State<Library> {
                     minimumSize: const Size.fromHeight(50), // NEW
                   ),
                   onPressed: () {
-                    setPrefNum('${index + 1}');
-                    store.setPerf(prefCityList[0]['${index + 1}']["name"]);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LibraryCity(
-                            prefNum: prefNum, prefCityList: prefCityList),
-                      ),
-                    );
+                    // setPrefNum('${index + 1}');
+                    // store.setPerf(prefCityList[0]['${index + 1}']["name"]);
+                    setState(() {
+                      prefNum = '${index + 1}';
+                      step++;
+                    });
                   },
                   child: Text(
                     prefCityList[0]['${index + 1}']["name"],
@@ -92,39 +242,12 @@ class _LibraryState extends State<Library> {
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class LibraryCity extends StatefulWidget {
-  const LibraryCity({Key? key, this.prefNum, this.prefCityList})
-      : super(key: key);
-  final prefNum;
-  final prefCityList;
-
-  @override
-  State<LibraryCity> createState() => _LibraryCityState();
-}
-
-class _LibraryCityState extends State<LibraryCity> {
-  @override
-  Widget build(BuildContext context) {
-    final Store store = Provider.of<Store>(context, listen: true);
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/calil_logo_black.png',
-          width: 170,
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: ListView.builder(
+      );
+    } else if (step == 1) {
+      return ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 2.5),
         shrinkWrap: true,
-        itemCount: widget.prefCityList[0][widget.prefNum]['city'].length,
+        itemCount: prefCityList[0][prefNum]['city'].length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
@@ -133,23 +256,17 @@ class _LibraryCityState extends State<LibraryCity> {
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    // primary: colorList[index % colorList.length],
+                    primary: colorList[index % colorList.length],
                     minimumSize: const Size.fromHeight(50), // NEW
                   ),
                   onPressed: () {
-                    store.setCity(widget.prefCityList[0][widget.prefNum]["city"]
-                        [index]['city']);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultLibrary(
-                            perfName: store.perfName, cityName: store.cityName),
-                      ),
-                    );
+                    // setCity(prefCityList[0][prefNum]["city"][index]['city']);
+                    setState(() {
+                      step++;
+                    });
                   },
                   child: Text(
-                    widget.prefCityList[0][widget.prefNum]["city"][index]
-                        ['city'],
+                    prefCityList[0][prefNum]["city"][index]['city'],
                     style: TextStyle(fontSize: 20),
                   ),
                 ),
@@ -157,7 +274,9 @@ class _LibraryCityState extends State<LibraryCity> {
             ),
           );
         },
-      ),
-    );
+      );
+    } else {
+      return Text("test");
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // import 'package:kariru/book.dart';
 import 'package:kariru/library.dart';
 import 'package:kariru/libraryGps.dart';
@@ -82,7 +83,7 @@ class _HomeMenuState extends State<HomeMenu> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => Library()),
+                      MaterialPageRoute(builder: (context) => Frame()),
                     );
                   },
                   child: SizedBox(
@@ -448,3 +449,60 @@ class _HomeMenuState extends State<HomeMenu> {
 //     }
 //   }
 // }
+class Frame extends StatefulWidget {
+  const Frame({Key? key}) : super(key: key);
+
+  @override
+  State<Frame> createState() => _FrameState();
+}
+
+class _FrameState extends State<Frame> {
+  int _currentIndex = 0;
+  final pageList = [
+    Library(),
+    LibraryGps(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/calil_logo_black.png',
+          width: 170,
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: pageList[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            label: ("地域"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: ("周り"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: ("お気入り"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: ("設定"),
+          ),
+        ],
+      ),
+    );
+  }
+}
